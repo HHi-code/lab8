92456d4cd0a78ff77023457884993fb7667937cf
Данил Абраменко <awidan2010@yandex.ru>
2024-10-04 12:57:42 +0300

    Добавил сообщения в зависимости от часа

--- lab8.pas
+++ lab8.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Println('Привет,Мир!');
+  var hour:= ReadInteger('Введите текущий час: ');
+  Assert(hour in 0..23);
+  case hour of 
+    4..10: println('Доброе утро, мир!');
+    11..16:println('Добрый день,мир!');
+    17..22:println('Добрый вечер, мир! ');
+    else Println('Доброй ночи,мир!');
+  end;
 end.
