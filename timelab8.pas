443e0d562808a899952db2d259031b45d0ccbb2d
Данил Абраменко <awidan2010@yandex.ru>
2024-10-04 13:14:42 +0300

    Добавил количество дней

--- timelab8.pas
+++ timelab8.pas
@@ -1,11 +1,11 @@
 ﻿begin
   var hour:=ReadInteger('Введите год: ');
   if (hour mod 4=0)and not(hour mod 100=0) and (hour mod 400<>0) then begin
-    Println('Год високосный');
+    Println('Год високосный, количество дней = 366');
     end
   else
     begin
-    Println('Год не високосный');
+    Println('Год не високосный, количество дней =365');
   end;
   
 end.
