773e1e8930970e8667e31c3f8427b341551578ad
Данил Абраменко <awidan2010@yandex.ru>
2024-10-04 13:11:06 +0300

    Проверка года на високосность

---
+++ timelab8.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var hour:=ReadInteger('Введите год: ');
+  if (hour mod 4=0)and not(hour mod 100=0) and (hour mod 400<>0) then begin
+    Println('Год високосный');
+    end
+  else
+    begin
+    Println('Год не високосный');
+  end;
+  
+end.
